319c9d4ff84a17443a7f3a48ee2510a2793278da
simplesports <simplesports.mail@gmail.com>
2017-05-26 11:47:02 -0600

    Add files via upload

--- Add_Projects.py
+++ Add_Projects.py
@@ -1,8 +1,10 @@
 import sys
-from PyQt5.QtCore import QCoreApplication
+from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
 from Add_Project_GUI import *
+import datetime
+import pdb
 
 
 
@@ -17,30 +19,46 @@ class AddProjects(QWidget):
     def setUpMainUiFunction(self):
         self.AddProjects.Button_Add_To_Table_Contacts.clicked.connect(self.addContacts)
         self.AddProjects.Button_Add_To_Table_Due_Dates.clicked.connect(self.addDates)
-        #pass
+        self.AddProjects.Button_Add_Project.clicked.connect(self.addProject)
+        self.AddProjects.Button_Contacts_Remove.clicked.connect(self.removeContact)
+
+        self.AddProjects.dateEdit.setCalendarPopup(True)
+        dt = datetime.date.today()
+        self.AddProjects.dateEdit.setDate(QDate(dt.year, dt.month, dt.day))
+
+        self.AddProjects.Button_Dates_Update_Table.hide()
+        self.AddProjects.Button_Contacts_Update_Table.hide()
+
 
     def addContacts(self):
-        contactName = self.AddProjects.UserInput_Main_Contact_Name.text()
-        contactNumber = self.AddProjects.UserInput_Main_Contact_PhoneNumber.text()
-        title = self.AddProjects.UserInput_Title.text()
-        company = self.AddProjects.UserInput_Company.text()
-        email = self.AddProjects.UserInput_Email.text()
+        if self.checkContactsAdd() == True:
+            contactName = self.AddProjects.UserInput_Main_Contact_Name.text()
+            contactNumber = self.AddProjects.UserInput_Main_Contact_PhoneNumber.text()
+            title = self.AddProjects.UserInput_Title.text()
+            company = self.AddProjects.UserInput_Company.text()
+            email = self.AddProjects.UserInput_Email.text()
 
-        if self.AddProjects.checkBox.isChecked():
-            mainContact = 'Yes'
-            self.AddProjects.checkBox.setEnabled(False)
-            self.AddProjects.checkBox.setChecked(False)
-        else:
-            mainContact = 'No'
+            if self.AddProjects.checkBox.isChecked():
+                mainContact = 'Yes'
+                self.AddProjects.checkBox.setEnabled(False)
+                self.AddProjects.checkBox.setChecked(False)
+            else:
+                mainContact = 'No'
 
-        rowPosition = self.AddProjects.tableWidget_Contacts.rowCount()
-        self.AddProjects.tableWidget_Contacts.insertRow(rowPosition)
-        self.AddProjects.tableWidget_Contacts.setItem(rowPosition,0,QTableWidgetItem(contactName))
-        self.AddProjects.tableWidget_Contacts.setItem(rowPosition,1,QTableWidgetItem(contactNumber))
-        self.AddProjects.tableWidget_Contacts.setItem(rowPosition,2,QTableWidgetItem(title))
-        self.AddProjects.tableWidget_Contacts.setItem(rowPosition,3,QTableWidgetItem(company))
-        self.AddProjects.tableWidget_Contacts.setItem(rowPosition,4,QTableWidgetItem(email))
-        self.AddProjects.tableWidget_Contacts.setItem(rowPosition,5,QTableWidgetItem(mainContact))
+            rowPosition = self.AddProjects.tableWidget_Contacts.rowCount()
+            self.AddProjects.tableWidget_Contacts.insertRow(rowPosition)
+            self.AddProjects.tableWidget_Contacts.setItem(rowPosition,0,QTableWidgetItem(contactName))
+            self.AddProjects.tableWidget_Contacts.setItem(rowPosition,1,QTableWidgetItem(contactNumber))
+            self.AddProjects.tableWidget_Contacts.setItem(rowPosition,2,QTableWidgetItem(title))
+            self.AddProjects.tableWidget_Contacts.setItem(rowPosition,3,QTableWidgetItem(company))
+            self.AddProjects.tableWidget_Contacts.setItem(rowPosition,4,QTableWidgetItem(email))
+            self.AddProjects.tableWidget_Contacts.setItem(rowPosition,5,QTableWidgetItem(mainContact))
+
+    def removeContact(self):
+        #Change it to select the whole row when clicked
+        indices = self.AddProjects.tableWidget_Contacts.selectionModel().selectedRows()
+        for index in sorted(indices):
+            self.AddProjects.tableWidget_Contacts.removeRow(index.row())
 
     def addDates(self):
         Description = self.AddProjects.UserInput_Description.text()
@@ -52,4 +70,40 @@ class AddProjects(QWidget):
         self.AddProjects.tableWidget_Dates.insertRow(rowPosition)
         self.AddProjects.tableWidget_Dates.setItem(rowPosition,0, QTableWidgetItem(Description))
         self.AddProjects.tableWidget_Dates.setItem(rowPosition,1, QTableWidgetItem(date))
-        #print(dateReformated)
+
+    def removeDate(self):
+        pass
+
+    def addProject(self):
+        check = self.checksToAddProject()
+        if check == True:
+            print('Project was added')
+
+    def checksToAddProject(self):
+
+        if self.AddProjects.tableWidget_Dates.rowCount() < 1:
+            print('need to add at least one date')
+            return False
+
+        if len(self.AddProjects.UserInput_Project_Number.text()) < 1:
+            print('need to add project Number')
+            return False
+        else:
+            try:
+                val = float(self.AddProjects.UserInput_Project_Number.text())
+            except:
+                print('The project number needs to be a number')
+                return False
+
+        if len(self.AddProjects.UserInput_Project_Name.text()) < 1:
+            print('need to add Project name')
+            return False
+
+        return True
+
+    def checkContactsAdd(self):
+        if len(self.AddProjects.UserInput_Main_Contact_Name.text()) < 1:
+            print('Need to have a contactName')
+            return False
+
+        return True
